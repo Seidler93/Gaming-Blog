248257c48debb6572014b41d92c9dc53b74425fe
AJ Seidler <ajseidler0526@gmail.com>
2024-01-31 13:39:26 -0600

    set up usercontext

--- gamingblog/pages/_app.js
+++ gamingblog/pages/_app.js
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
+import { useUserData } from '../lib/hooks';
+import { UserContext } from "@/lib/context";
+
 
 export default function App({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
-    <UserContext.Provider value={{ user: {}, username: 'jeff' }}>
+    <UserContext.Provider value={userData}>
       <Navbar/>
       <Component {...pageProps} />;
       <Toaster/>
